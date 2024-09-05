c677dbd2016d978b6eb61a4911d6bc14ca9e60ed
neerajsharma0392 <neeraj.sharma0392@gmail.com>
2024-09-05 16:26:13 +0000

    added getuserchat endpoint

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,21 @@
-import {getUsers as getUsersFromDB} from './../database';
+import {getUsers as getUsersFromDB,getUserChats as getUserChatsFromDB} from './../database';
 
 export const getUsers = {
     method: "get",
     path: "/users",
     requestProcessor: async (req, res) => {
         const users = await getUsersFromDB();
+        res.status(200).json(users);
+    }
+}
 
-
+export const getUserChats={
+    method: "get",
+    path: "/users/:id/chats",
+    requestProcessor: async (req, res) => {
+        const id= req.params.id;
+        const chats = await getUserChatsFromDB(id);
+        res.status(200).json(chats);
+    }
+}
 

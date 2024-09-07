1fec56d740088295f1c6e2a93ba32227993a749a
neerajsharma0392 <neeraj.sharma0392@gmail.com>
2024-09-07 16:00:33 +0000

    added socket io

--- src/server.js
+++ src/server.js
@@ -3,25 +3,57 @@ import * as admin from 'firebase-admin';
 import firebaseCredentials from './firebase-credentials.json';
 import cors from "cors";
 import {protectRoute,routes} from './controller';
-import {database} from "./database";
+import {database, getChats} from "./database";
+import socketIO from 'socket.io';
+import http from 'http';
+import {addMessage} from "./database/addMessage";
 
- admin.initializeApp({credential: admin.credential.cert(firebaseCredentials)});
+admin.initializeApp({credential: admin.credential.cert(firebaseCredentials)});
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.use(express.json());
 
 // register routes
-// app.method(path, hadnler)
+// app.method(path, handler)
 routes.forEach(route => {
     app[route.method](route.path, protectRoute, route.requestProcessor);
 })
 
 
+const server= http.createServer(app);
+const io =socketIO(server,{
+    cors:{
+        origin:"*"
+    }
+})
+
+
+io.use(async(socket, next)=>{
+
+    const user = await admin.auth().verifyIdToken(socket.handshake.query.token);
+    next();
+})
+
+
+io.on("connection", async(socket)=>{
+
+    socket.on("sendMessage", async function(data){ // message, userId, chatId
+        await addMessage(data.message, data.userId, data.chatId); // Addd to Mongo
+
+        const messages = await getChats(data.chatId);
+
+        io.emit("updatedChats",{messages, chatId: data.chatId});
+    })
+})
+
+
 const start = async () => {
     await database.connect('mongodb://127.0.0.1:27017');
-    app.listen(8080, () => {
+    server.listen(8080, () => {
         console.log("Listening on port 8080");
     })
 };
